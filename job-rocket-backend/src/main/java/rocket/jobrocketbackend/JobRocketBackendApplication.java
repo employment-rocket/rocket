7cfc940bb381571fa1c1a869b60c38eb0aa53cf7
Jeong gi seok <wjdrltjr5@naver.com>
2024-11-23 14:42:45 +0900

    init : 기본세팅

---
+++ job-rocket-backend/src/main/java/rocket/jobrocketbackend/JobRocketBackendApplication.java
@@ -0,0 +1,13 @@
+package rocket.jobrocketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRocketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRocketBackendApplication.class, args);
+    }
+
+}
---
+++ job-rocket-backend/src/test/java/rocket/jobrocketbackend/JobRocketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rocket.jobrocketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRocketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
