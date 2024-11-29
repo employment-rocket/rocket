023253d753f942fe159744acd1a460e36507d5e9
Jeong gi seok <wjdrltjr5@naver.com>
2024-11-29 19:37:23 +0900

    init : submodule

---
+++ job-rocket-backend/src/main/java/rocket/jobrocketbackend/TestController.java
@@ -0,0 +1,25 @@
+package rocket.jobrocketbackend;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @Value("${my.name}")
+    String test;
+    @PostConstruct
+    public void init() {
+        log.info("test = {}",test);
+    }
+
+    @GetMapping("/")
+    public String test() {
+        return test;
+    }
+}
