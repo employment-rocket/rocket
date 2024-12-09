763d40449b72c24433bd9ea9673fbf56b20c56fe
Jeong gi seok <wjdrltjr5@naver.com>
2024-12-09 20:33:02 +0900

    design : 취준 도움 사이트 #3

--- job-rocket-frontend/src/pages/Site.jsx
+++ job-rocket-frontend/src/pages/Site.jsx
@@ -1,7 +1,238 @@
 import React from "react";
 
 const Site = () => {
-	return <div>취준도움사이트</div>;
+	return (
+		<div
+			className="flex justify-center content-center mt-20 space-x-11"
+			style={{ fontFamily: "CookieRegular" }}
+		>
+			<div className="flex flex-col w-[15%] items-center ">
+				<div
+					className="p-5 justify-center rounded-[10px] w-full flex"
+					style={{
+						backgroundColor: "#3F83F8",
+						color: "white",
+						fontFamily: "CookieBold",
+					}}
+				>
+					<div>채용 공고</div>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.jobkorea.co.kr/" target="_blank">
+						잡코리아
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a
+						href="https://www.saramin.co.kr/zf_user/"
+						target="_blank"
+					>
+						사람인
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.wanted.co.kr/" target="_blank">
+						원티드
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.rocketpunch.com/" target="_blank">
+						로켓펀치
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://jumpit.saramin.co.kr/" target="_blank">
+						점핏
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://jasoseol.com/" target="_blank">
+						자소설닷컴
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://zighang.com/" target="_blank">
+						직행
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.jobplanet.co.kr/job" target="_blank">
+						잡플래닛
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.catch.co.kr/" target="_blank">
+						캐치
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.superookie.com/" target="_blank">
+						슈퍼루키
+					</a>
+				</div>
+			</div>
+
+			<div className="flex flex-col w-[15%] items-center ">
+				<div
+					className="p-5 justify-center rounded-[10px] w-full flex"
+					style={{
+						backgroundColor: "#3F83F8",
+						color: "white",
+						fontFamily: "CookieBold",
+					}}
+				>
+					<div>취준 커뮤니티</div>
+				</div>
+				<div className="p-4 ">
+					<a href="https://linkareer.com/" target="_blank">
+						링커리어
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://cafe.naver.com/specup" target="_blank">
+						스펙업
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://cafe.naver.com/dokchi" target="_blank">
+						독취사
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://comento.kr/?index" target="_blank">
+						코멘토
+					</a>
+				</div>
+			</div>
+
+			<div className="flex flex-col w-[15%] items-center ">
+				<div
+					className="p-5 justify-center rounded-[10px] w-full flex"
+					style={{
+						backgroundColor: "#3F83F8",
+						color: "white",
+						fontFamily: "CookieBold",
+					}}
+				>
+					<div>기업 분석</div>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.jobplanet.co.kr/job" target="_blank">
+						잡플래닛
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.catch.co.kr/" target="_blank">
+						캐치
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://dart.fss.or.kr/" target="_blank">
+						DART
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://consensus.hankyung.com/" target="_blank">
+						한경 컨센서스
+					</a>
+				</div>
+			</div>
+
+			<div className="flex flex-col w-[15%] items-center ">
+				<div
+					className="p-5 justify-center rounded-[10px] w-full flex"
+					style={{
+						backgroundColor: "#3F83F8",
+						color: "white",
+						fontFamily: "CookieBold",
+					}}
+				>
+					<div>대외 활동</div>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.superookie.com/" target="_blank">
+						슈퍼루키
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://linkareer.com/" target="_blank">
+						링커리어
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.allforyoung.com/" target="_blank">
+						요즘것들
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.ssgsag.kr/" target="_blank">
+						슥삭
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.campuspick.com/" target="_blank">
+						캠퍼스픽
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a
+						href="https://www.thinkcontest.com/thinkgood/index.do"
+						target="_blank"
+					>
+						씽굿
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.all-con.co.kr/" target="_blank">
+						올콘
+					</a>
+				</div>
+			</div>
+
+			<div className="flex flex-col w-[15%] items-center ">
+				<div
+					className="p-5 justify-center rounded-[10px] w-full flex"
+					style={{
+						backgroundColor: "#3F83F8",
+						color: "white",
+						fontFamily: "CookieBold",
+					}}
+				>
+					<div>코딩 테스트</div>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.acmicpc.net/" target="_blank">
+						백준
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://programmers.co.kr/" target="_blank">
+						프로그래머스
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.softeer.ai/" target="_blank">
+						소프티어
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://leetcode.com/" target="_blank">
+						리트코드
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://level.goorm.io/" target="_blank">
+						구름
+					</a>
+				</div>
+				<div className="p-4 ">
+					<a href="https://www.codetree.ai/landing" target="_blank">
+						코드트리
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Site;
