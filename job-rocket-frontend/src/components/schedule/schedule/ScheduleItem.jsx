37bfe5a4ca5a3c6c3451c11fa52f64ed15644b74
Jeong gi seok <wjdrltjr5@naver.com>
2024-12-17 00:18:40 +0900

    design : delete modal

--- job-rocket-frontend/src/components/schedule/schedule/ScheduleItem.jsx
+++ job-rocket-frontend/src/components/schedule/schedule/ScheduleItem.jsx
@@ -1,27 +1,39 @@
-import React from "react";
+import { React, useState } from "react";
 import del from "../../../assets/delete.png";
+import DeleteModal from "./DeleteModal";
 
 const ScheduleItem = ({ item, droppableId, handleDelete }) => {
-	return (
-		<div className="flex flex-col w-full py-2 space-y-3">
-			<div className="flex justify-between items-center">
-				<div>{item.title}</div>
+	const [isModalOpen, setModalOpen] = useState(false);
 
-				<img
-					src={del}
-					alt="삭제"
-					onClick={() => handleDelete(item.id, droppableId)}
-					className="cursor-pointer"
-				/>
+	return (
+		<>
+			<div className="flex flex-col w-full py-2 space-y-3">
+				<div className="flex justify-between items-center">
+					<div>{item.title}</div>
+
+					<img
+						src={del}
+						alt="삭제"
+						onClick={() => setModalOpen(true)}
+						className="cursor-pointer"
+					/>
+				</div>
+				<div
+					className="flex justify-between"
+					style={{ fontSize: "0.8rem" }}
+				>
+					<div>{item.type}</div>
+					<div>~{item.dueDate}</div>
+				</div>
 			</div>
-			<div
-				className="flex justify-between"
-				style={{ fontSize: "0.8rem" }}
-			>
-				<div>{item.type}</div>
-				<div>~{item.dueDate}</div>
-			</div>
-		</div>
+			<DeleteModal
+				isOpen={isModalOpen}
+				handleDelete={handleDelete}
+				id={item.id}
+				droppableId={droppableId}
+				onCancel={() => setModalOpen(false)}
+			/>
+		</>
 	);
 };
 
