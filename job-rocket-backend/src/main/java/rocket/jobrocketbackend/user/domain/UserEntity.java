e6ed05ff5fc28b0f5f5ee188265d5576f0df0d0d
Chaewon An <woneveryday@naver.com>
2024-12-16 15:06:41 +0900

    feat : login

--- job-rocket-backend/src/main/java/rocket/jobrocketbackend/user/domain/UserEntity.java
+++ job-rocket-backend/src/main/java/rocket/jobrocketbackend/user/domain/UserEntity.java
@@ -1,4 +1,39 @@
 package rocket.jobrocketbackend.user.domain;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import rocket.jobrocketbackend.common.entity.Role;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name="member")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String nickname;
+    private String profile;
+    private String setemail;
+    private String refreshToken;
+
+    @Builder
+    public UserEntity(Long id, String email, Role role, String nickname, String profile, Boolean setemail, String refreshToken){
+        this.id=id;
+        this.email=email;
+        this.role=role;
+        this.nickname=nickname;
+        this.profile=profile;
+        this.setemail= String.valueOf(setemail);
+        this.refreshToken=refreshToken;
+    }
 }
--- job-rocket-backend/src/main/java/rocket/jobrocketbackend/user/repository/UserRepository.java
+++ job-rocket-backend/src/main/java/rocket/jobrocketbackend/user/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package rocket.jobrocketbackend.user.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rocket.jobrocketbackend.user.domain.UserEntity;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
