3cdffe464fb5129f652a3ee23fec6947342248b7
Jeong gi seok <wjdrltjr5@naver.com>
2025-01-02 22:23:32 +0900

    design : 비율수정

--- job-rocket-frontend/src/components/schedule/ScheduleHome.jsx
+++ job-rocket-frontend/src/components/schedule/ScheduleHome.jsx
@@ -5,10 +5,10 @@ import Calendar from "./calendar/Calendar";
 const ScheduleHome = () => {
 	return (
 		<div className="flex w-full h-full justify-center space-2">
-			<div className="w-full">
+			<div className="w-[40%]">
 				<ScheduleMain />
 			</div>
-			<div className="w-full h-full">
+			<div className="w-[60%] h-full">
 				<Calendar />
 			</div>
 		</div>
