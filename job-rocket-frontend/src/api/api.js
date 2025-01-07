f5355cd0569df9447e17b0ed95ca06f9cde9abed
jang643 <jang643@naver.com>
2025-01-07 22:22:24 +0900

    Feature: gpt 연동 및 로딩

--- job-rocket-frontend/src/api/api.js
+++ job-rocket-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const api = axios.create({
 	baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
-	timeout: 2000,
+	timeout: 30000,
 	headers: {
 		"Content-Type": "application/json",
 	},
