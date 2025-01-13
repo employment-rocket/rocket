643cd9d72313c71daec92c720e9c921c3e288d7e
Jeong gi seok <wjdrltjr5@naver.com>
2025-01-13 18:18:51 +0900

    design : aside

--- job-rocket-frontend/src/pages/Board.jsx
+++ job-rocket-frontend/src/pages/Board.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Board = () => {
-	return <div>게시판 화면</div>;
+	return (
+		<div className="flex">
+			<div>측면</div>
+			<div>게시판 화면</div>
+		</div>
+	);
 };
 
 export default Board;
