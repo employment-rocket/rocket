332e4a531c9854006a94601d8a35c6fbabacfa8b
Jeong gi seok <wjdrltjr5@naver.com>
2025-01-20 19:03:50 +0900

    design : comment icon add

--- job-rocket-frontend/src/components/board/free/FreeBoardView.jsx
+++ job-rocket-frontend/src/components/board/free/FreeBoardView.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useParams } from "react-router";
+import FreeComment from "./comment/FreeComment";
+import comment from "../../../assets/comment.png";
 
 const FreeBoardView = () => {
 	const mock = {
@@ -19,10 +21,10 @@ const FreeBoardView = () => {
 			<div className="flex justify-between items-center">
 				<div style={{ fontSize: "1.3rem" }}>{mock.title}</div>
 				<div className="flex gap-2 items-center">
-					<div className="bg-blue-500 text-white p-2 rounded-lg">
+					<div className="bg-blue-500 text-white p-2 px-6 rounded-lg">
 						수정
 					</div>
-					<div className="border text-red-500 p-2 rounded-lg">
+					<div className="border text-red-500 p-2 px-6 rounded-lg">
 						삭제
 					</div>
 				</div>
@@ -31,8 +33,17 @@ const FreeBoardView = () => {
 				<div>{mock.author}</div>
 				<div>{mock.post_date}</div>
 			</div>
+
 			<hr />
 			<div className="min-h-[16rem]">{mock.content}</div>
+			<div className="flex gap-2 items-center">
+				<img
+					src={comment}
+					alt="댓글 아이콘"
+					className="h-[32px] w-[32px]"
+				/>
+				<div>1</div>
+			</div>
 			<hr />
 			<div className="flex border rounded-lg p-3 space-x-3 w-full">
 				<input
@@ -40,10 +51,11 @@ const FreeBoardView = () => {
 					placeholder="댓글을 달아주세요"
 					className="grow"
 				/>
-				<div className="bg-blue-500 text-white rounded-lg p-2">
+				<div className="bg-blue-500 text-white rounded-lg p-2 px-6">
 					등록
 				</div>
 			</div>
+			<FreeComment />
 		</div>
 	);
 };
---
+++ job-rocket-frontend/src/components/board/free/comment/FreeComment.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FreeComment = () => {
+	return (
+		<div className="flex flex-col border rounded-lg p-3 gap-2">
+			<div className="flex justify-between ">
+				<div>청년백수</div>
+				<div>2025-01-23</div>
+			</div>
+			<div>
+				<hr />
+			</div>
+			<div>날씨가 추우니 다음에 가죠</div>
+		</div>
+	);
+};
+
+export default FreeComment;
