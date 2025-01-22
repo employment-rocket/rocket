632f684d3b4970a7e923312bac0ba2cea94fb073
Chaewon An <woneveryday@naver.com>
2025-01-23 00:49:45 +0900

    fix: 누락된 파일 추가

---
+++ job-rocket-backend/src/main/java/rocket/jobrocketbackend/common/entity/SocialType.java
@@ -0,0 +1,10 @@
+package rocket.jobrocketbackend.common.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SocialType {
+    KAKAO, NAVER
+}
