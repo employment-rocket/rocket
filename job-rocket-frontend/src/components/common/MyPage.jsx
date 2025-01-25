320eb490f525b59955258e04afb291b52931dab5
Chaewon An <woneveryday@naver.com>
2025-01-26 00:37:41 +0900

    fix: 마이페이지 디자인 수정

--- job-rocket-frontend/src/components/common/MyPage.jsx
+++ job-rocket-frontend/src/components/common/MyPage.jsx
@@ -10,6 +10,7 @@ import {
 	updatedAllowAlarm,
 } from "../../api/user/UserApi";
 import useProfileStore from "../../store/profileImageStore";
+import camera from "../../assets/camera.png";
 
 const MyPage = ({onClose, onNavigate}) => {
 	const navigate = useNavigate();
@@ -104,106 +105,101 @@ const MyPage = ({onClose, onNavigate}) => {
 
 	return (
 
-		<div ref={myPageRef} className="absolute top-14 right-6 flex items-center justify-center bg-white z-50">
-			<div
-				className="w-[550px] h-[600px] rounded-2xl border border-gray-300 bg-white shadow-md flex flex-col justify-center items-center"
-				style={{ fontFamily: "CookieBold" }}
-			>
-				<div className="flex items-center mb-8 w-full px-6">
-					<div className="w-[164px] h-[50px] text-xl text-black border border-gray-300 text-center rounded-lg flex items-center justify-center">
-						이메일
-					</div>
-					<div className="flex-1 text-xl text-black ml-4">
-						{email}
-					</div>
-				</div>
+<div ref={myPageRef} className="absolute top-14 right-3 flex items-center justify-center bg-white z-50">
 
-				<div className="flex items-center mb-8 w-full px-6">
-					<div className="w-[164px] h-[50px] text-xl text-black border border-gray-300 text-center rounded-lg flex items-center justify-center">
-						프로필 사진
-					</div>
-					<img
-						className="w-40 h-40 rounded-full ml-8"
-						src={profile === "default" ? logo : profile}
-						alt="Profile"
-					/>
-					<input
-						type="file"
-						id="profile-upload"
-						accept="image/*"
-						className="hidden"
-						onChange={async (e) => {
-							if (e.target.files && e.target.files[0]) {
-								handleFileUpload(e.target.files[0]);
-							}
-						}}
-					/>
-					<button
-						className="w-[100px] h-[50px] text-xl text-blue-500 border border-gray-300 text-center rounded-lg flex items-center justify-center ml-auto"
-						onClick={() =>
-							document.getElementById("profile-upload").click()
-						}
-					>
-						수정
-					</button>
-				</div>
+      <div
+        className="w-[450px] h-[600px] rounded-2xl border border-gray-300 bg-white shadow-md flex flex-col justify-center items-center"
+        style={{ fontFamily: "CookieBold" }}
+      >
+			<div className="w-4/5 h-px bg-gray-300 mt-1"></div>
+        <div className="flex items-center mb-8 w-full px-6 justify-center mr-8 relative mt-6">
+          <img
+            className="w-40 h-40 rounded-full ml-8 border border-gray-300"
+            src={profile === "default" ? logo : profile}
+            alt="Profile"
+          />
+          <input
+            type="file"
+            id="profile-upload"
+            accept="image/*"
+            className="hidden"
+            onChange={async (e) => {
+              if (e.target.files && e.target.files[0]) {
+                handleFileUpload(e.target.files[0]);
+              }
+            }}
+          />
+		  <img 
+		  className="w-7 h-7 rounded-full ml-10 absolute bottom-0 right-[110px]"
+		  src={camera}
+		  alt="camera icon"
+  			onClick={() => document.getElementById("profile-upload").click()}
+		  />
 
-				<div className="flex items-center mb-8 w-full px-6">
-					<div className="w-[164px] h-[50px] text-xl text-black border border-gray-300 text-center rounded-lg flex items-center justify-center">
-						닉네임
-					</div>
-					<div className="flex-1 text-xl text-black ml-4">
-						{nickname}
-					</div>
-				</div>
 
-				<div className="flex items-center mb-6 w-full px-6">
-					<div className="w-[164px] h-[50px] text-xl text-black border border-gray-300 text-center rounded-lg flex items-center justify-center">
-						알람 수신 동의
-					</div>
-					<div className="ml-4">
-						<label className="relative inline-flex items-center cursor-pointer">
-							<input
-								type="checkbox"
-								className="sr-only peer"
-								checked={allowAlarm}
-								onChange={handleAllowAlarmChange}
-							/>
-							<div className="w-16 h-8 bg-gray-300 rounded-full peer-checked:bg-blue-500 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 transition duration-200"></div>
-							<span className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full peer-checked:translate-x-7 transform transition duration-200"></span>
-						</label>
-					</div>
-				</div>
+		
+        </div>
 
-				<div className="flex items-center w-full px-6 mb-1">
-					<div className="w-[164px] h-[50px] text-xl text-black border border-gray-300 text-center rounded-lg flex items-center justify-center">
-						이메일 수신 동의
-					</div>
-					<div className="ml-4">
-						<label className="relative inline-flex items-center cursor-pointer">
-							<input
-								type="checkbox"
-								className="sr-only peer"
-								checked={allowEmail}
-								onChange={handleAllowEmailChange}
-							/>
-							<div className="w-16 h-8 bg-gray-300 rounded-full peer-checked:bg-blue-500 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 transition duration-200"></div>
-							<span className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full peer-checked:translate-x-7 transform transition duration-200"></span>
-						</label>
-					</div>
-				</div>
-				<div className="flex items-center justify-end w-full px-6 mb-1 ">
-  <button
-    className="w-[100px] h-[50px] mt-4 text-center text-red-500 text-xl mr-3 border border-gray-300 text-center rounded-lg "
-    onClick={() => onNavigate("logout")}
-  >
-    로그아웃
-  </button>
-</div>
-			</div>
-		</div>
-		//)
-	);
+        <div className="flex items-center mb-8 w-full px-6">
+          <div className="flex-1 text-3xl text-black text-center">
+            {nickname}
+          </div>
+        </div>
+
+        
+        <div className="flex items-center mb-8 w-full px-6">
+          <div className="flex-1 text-xl text-gray-500 ml-4 text-center">
+            {email}
+          </div>
+        </div>
+
+        <div className="flex items-center mb-3 w-full px-6">
+          <div className="w-[164px] h-[50px] ml-10 text-xl text-black border border-gray-300 text-center rounded-lg flex items-center justify-center">
+            알람 수신 동의
+          </div>
+          <div className="ml-4">
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                checked={allowAlarm}
+                onChange={handleAllowAlarmChange}
+              />
+              <div className="w-32 h-10 bg-gray-300 rounded-full peer-checked:bg-blue-500 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 transition duration-200"></div>
+              <span className="absolute left-1 top-1 w-8 h-8 bg-white rounded-full peer-checked:translate-x-20 transform transition duration-200"></span>
+            </label>
+          </div>
+        </div>
+
+        <div className="flex items-center w-full px-6 mb-1">
+          <div className="w-[164px] h-[50px] text-xl ml-10 text-black border border-gray-300 text-center rounded-lg flex items-center justify-center">
+            이메일 수신 동의
+          </div>
+          <div className="ml-4">
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                checked={allowEmail}
+                onChange={handleAllowEmailChange}
+              />
+              <div className="w-32 h-10 bg-gray-300 rounded-full peer-checked:bg-blue-500 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-300 transition duration-200"></div>
+              <span className="absolute left-1 top-1 w-8 h-8 bg-white rounded-full peer-checked:translate-x-20 transform transition duration-200"></span>
+            </label>
+          </div>
+        </div>
+		<div className="w-4/5 h-px bg-gray-300 mt-5"></div>
+        <div className="flex items-center justify-center w-full px-6">
+          <button
+            className="w-[200px] h-[45px] mt-5 text-center text-red-500 text-xl border-2 border-red-500 text-center rounded-lg"
+            onClick={() => onNavigate("logout")}
+          >
+            로그아웃
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
