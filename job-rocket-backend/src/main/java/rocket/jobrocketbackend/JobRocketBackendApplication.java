59ec7d274f48371e5e029145800f4c34966f8fbc
Jeong gi seok <wjdrltjr5@naver.com>
2025-01-29 09:35:10 +0900

    chore : 주석제거

--- job-rocket-backend/src/main/java/rocket/jobrocketbackend/JobRocketBackendApplication.java
+++ job-rocket-backend/src/main/java/rocket/jobrocketbackend/JobRocketBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-//@EnableMongoRepositories(basePackages = "rocket.jobrocketbackend.profile.profile.repository")
 public class JobRocketBackendApplication {
 
     public static void main(String[] args) {
