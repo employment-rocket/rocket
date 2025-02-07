9c5653dcc2a0a4f6876d5e4a37bace3cb6039298
Jeong gi seok <wjdrltjr5@naver.com>
2025-02-07 19:10:32 +0900

    design : main board

--- job-rocket-frontend/src/components/board/main/BoardItem.jsx
+++ job-rocket-frontend/src/components/board/main/BoardItem.jsx
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from "react";
 
-const BoardItem = () => {
-	const item = {
+const BoardItem = ({ item }) => {
+	const mock = {
 		title: "공지) 공지입니다.",
 		content: "게시판 이용 수칙입니다.",
 		profile: "default.png",
@@ -10,23 +10,23 @@ const BoardItem = () => {
 	};
 
 	const img = `${import.meta.env.VITE_API_BASE_URL}/board/free/temp/${
-		item.profile
+		mock.profile
 	}`;
 	return (
 		<div
 			className="flex flex-col border shadow-sm shadow-gray-500 p-1
 		 rounded-lg space-y-3"
 		>
-			<div className="line-clamp-1">{item.title}</div>
+			<div className="line-clamp-1">{mock.title}</div>
 
 			<hr />
-			<div className="line-clamp-3 leading-6">{item.content}</div>
+			<div className="line-clamp-3 leading-6">{mock.content}</div>
 			<div className="flex justify-between">
 				<div className="flex items-center">
 					<img className="w-[1.5rem]" src={img} />
-					{item.nickName}
+					{mock.nickName}
 				</div>
-				<div className="text-gray-500">{item.postDate}</div>
+				<div className="text-gray-500">{mock.postDate}</div>
 			</div>
 		</div>
 	);
--- job-rocket-frontend/src/components/board/main/MainBoard.jsx
+++ job-rocket-frontend/src/components/board/main/MainBoard.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from "react";
-import NoticeZone from "./NoticeZone";
+import BoardZone from "./BoardZone";
 import { useHeaderHeightStore } from "../../../store/headerHeightStore";
 
 const MainBoard = () => {
 	const headerHeight = useHeaderHeightStore((state) => state.headerHeight);
-	console.log(headerHeight);
+	const TYPE = {
+		NOTICE: "notice",
+		FREE: "free",
+		QA: "qa",
+		REVIEW: "review",
+	};
+
 	return (
 		<div
 			className="flex flex-col bg-slate-300 border justify-center"
@@ -15,10 +21,10 @@ const MainBoard = () => {
 		>
 			<div></div>
 			<div className="grid grid-cols-2  bg-white gap-3 gap-y-5 h-full mt-32 p-5">
-				<NoticeZone />
-				<NoticeZone />
-				<NoticeZone />
-				<NoticeZone />
+				<BoardZone type={TYPE.NOTICE} />
+				<BoardZone type={TYPE.FREE} />
+				<BoardZone type={TYPE.QA} />
+				<BoardZone type={TYPE.REVIEW} />
 			</div>
 		</div>
 	);
