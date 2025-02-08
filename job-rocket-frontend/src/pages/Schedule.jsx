dbb534d6236e00b1f64bc29754b0d42668a80e9d
Jeong gi seok <wjdrltjr5@naver.com>
2025-02-08 17:51:43 +0900

    design : schedule design change

--- job-rocket-frontend/src/pages/Schedule.jsx
+++ job-rocket-frontend/src/pages/Schedule.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import Calendar from "../components/schedule/calendar/Calendar";
 import { useHeaderHeightStore } from "../store/headerHeightStore";
 
 const Schedule = () => {
 	const navigate = useNavigate();
+	const location = useLocation();
 	const headerHeight = useHeaderHeightStore((state) => state.headerHeight);
+	const isActive = location.pathname === "/schedule";
+
 	return (
 		<div
 			className="flex flex-col w-full h-screen"
@@ -15,18 +18,23 @@ const Schedule = () => {
 			}}
 		>
 			<div className="flex w-full h-full justify-center items-center space-2 p-4">
-				<div className="flex flex-col px-2 items-center w-[40%] h-full rounded-lg border-2 border-gray-300 space-y-3">
+				<div className="flex flex-col px-2 items-center w-[40%] h-full rounded-lg border-2 border-gray-300 ">
 					<div className="flex w-full items-center justify-around p-3">
 						<div
-							className="text-xl cursor-pointer"
+							className={`w-full text-xl cursor-pointer text-center ${
+								isActive ? "text-black" : "text-gray-300"
+							}`}
 							onClick={() => navigate("/schedule")}
 						>
 							내 일정
 						</div>
-
+						<div className="w-1 h-full bg-gray-300"></div>
 						<div
-							className="text-xl cursor-pointer"
+							className={`w-full text-xl cursor-pointer text-center ${
+								isActive ? "text-gray-300" : "text-black"
+							}`}
 							onClick={() => navigate("/schedule/statistics")}
+							style={{ text: `${isActive} ? gray : blue` }}
 						>
 							내 통계
 						</div>
